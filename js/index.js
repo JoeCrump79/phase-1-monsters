ab23efb60a04a4ca44424f5fb4a408052aa1e50d
Joseph Crump <josephcrump79@yahoo.com>
2024-10-02 09:42:05 -0500

    Create HTML for monsters and implements forward/back button

--- js/index.js
+++ js/index.js
@@ -2,21 +2,50 @@ const createMonster = document.getElementById('create-monster')
 const monsterContainter = document.getElementById('monster-container')
 const back = document.getElementById("back")
 const forward = document.getElementById("forward")
+let pageNumber = 1
 
-window.addEventListener('load', (e) => {
-    // TODO: Add paging
-    fetch(`http://localhost:3000/monsters/?_limit=50`)
-    .then(response => {
-        if (!response.ok) {
-            console.log(response.status)
-        }
-        return response.json()
-    })
-    .then(results => {
-        results.forEach(monster => {
-            console.log(monster)
-
-            // TODO: Create elements to show monster 
+function grabAndaddMonsters() {
+    console.log(pageNumber)
+    fetch(`http://localhost:3000/monsters/?_limit=2&_page=${pageNumber}`)
+        .then(response => {
+            if (!response.ok) {
+                console.log(response.status)
+            }
+            return response.json()
         })
-    })
+        .then(results => {
+            monsterContainter.innerHTML = ""
+            results.forEach(monster => {
+                //console.log(monster)
+                const monsterDiv = document.createElement('div')
+                monsterContainter.append(monsterDiv)
+                const monsterH2 = document.createElement('h2')
+                monsterH2.textContent = monster.name
+                monsterDiv.append(monsterH2)
+                const monsterH4 = document.createElement('h4')
+                monsterDiv.append(monsterH4)
+                monsterH4.textContent = `Age: ${monster.age}`
+                const monsterP = document.createElement('p')
+                monsterDiv.append(monsterP)
+                monsterP.textContent = `Bio: ${monster.description}`
+            })
+        })
+}
+
+window.addEventListener('load', grabAndaddMonsters)
+
+forward.addEventListener('click', (e) => {
+    e.preventDefault
+    console.log('forward')
+    pageNumber = pageNumber + 1
+    grabAndaddMonsters()
+})
+
+back.addEventListener('click', (e) => {
+    e.preventDefault
+    console.log('back')
+    if (pageNumber > 1) {
+        pageNumber = pageNumber - 1
+    }
+    grabAndaddMonsters()
 })
