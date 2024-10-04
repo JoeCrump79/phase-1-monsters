56b2ba04136b2dd9927b34144d1933cd29b3261c
Joseph Crump <josephcrump79@yahoo.com>
2024-10-04 08:49:06 -0500

    phase-1-monsters

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ let pageNumber = 1
 
 function grabAndaddMonsters() {
     console.log(pageNumber)
-    fetch(`http://localhost:3000/monsters/?_limit=2&_page=${pageNumber}`)
+    fetch(`http://localhost:3000/monsters/?_limit=50&_page=${pageNumber}`)
         .then(response => {
             if (!response.ok) {
                 console.log(response.status)
@@ -14,6 +14,7 @@ function grabAndaddMonsters() {
             return response.json()
         })
         .then(results => {
+            console.log(results)
             monsterContainter.innerHTML = ""
             results.forEach(monster => {
                 //console.log(monster)
@@ -35,17 +36,80 @@ function grabAndaddMonsters() {
 window.addEventListener('load', grabAndaddMonsters)
 
 forward.addEventListener('click', (e) => {
-    e.preventDefault
+    e.preventDefault()
     console.log('forward')
     pageNumber = pageNumber + 1
     grabAndaddMonsters()
 })
 
 back.addEventListener('click', (e) => {
-    e.preventDefault
+    e.preventDefault()
     console.log('back')
     if (pageNumber > 1) {
         pageNumber = pageNumber - 1
     }
     grabAndaddMonsters()
+})
+
+const monsterForm = document.createElement('form')
+monsterForm.id = 'monster-form' 
+createMonster.append(monsterForm)
+const nameInput = document.createElement('input')
+nameInput.id = 'name'
+nameInput.placeholder = 'name'
+monsterForm.append(nameInput)
+const ageInput = document.createElement('input')
+ageInput.id = 'age'
+ageInput.placeholder = 'age'
+monsterForm.append(ageInput)
+const descriptionInput = document.createElement('input')
+descriptionInput.id = 'description'
+descriptionInput.placeholder = 'description'
+monsterForm.append(descriptionInput)
+const createButton = document.createElement('button')
+createButton.textContent = 'Create'
+monsterForm.append(createButton)
+
+monsterForm.addEventListener('submit', (e) => {
+        console.log(nameInput.value)
+        console.log(descriptionInput.value)
+        console.log(ageInput.value)
+        e.preventDefault()
+        const configurationObject = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+                name: nameInput.value,
+                description: descriptionInput.value,
+                age: parseFloat(ageInput.value)
+                
+            }),
+        }
+        fetch("http://localhost:3000/monsters", configurationObject)
+        .then(response => {
+            if (!response.ok) {
+                console.log(response.status)
+            }
+            return response.json()
+        })
+        .then(monster => {
+            const monsterDiv = document.createElement('div')
+                monsterContainter.append(monsterDiv)
+                const monsterH2 = document.createElement('h2')
+                monsterH2.textContent = monster.name
+                monsterDiv.append(monsterH2)
+                const monsterH4 = document.createElement('h4')
+                monsterDiv.append(monsterH4)
+                monsterH4.textContent = `Age: ${monster.age}`
+                const monsterP = document.createElement('p')
+                monsterDiv.append(monsterP)
+                monsterP.textContent = `Bio: ${monster.description}`
+            })
+        .catch(function (error) {
+            alert("Bad things! Ragnarők!");
+            console.log(error.message);
+          });
 })
